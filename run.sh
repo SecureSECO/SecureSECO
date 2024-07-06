2c1d679c510cbaa3d6de5e31817f2ba4d3638287
martijnv <martijnvoordouw@gmail.com>
2024-07-06 15:54:15 +0200

    removed deprecated docker compose version tag, removed hardcoded github api key

--- run.sh
+++ run.sh
@@ -11,9 +11,6 @@ fi
 
 echo 'Starting up SecureSECO'
 
-# pat=$(echo 'Z2hwX0JSR3UxQjR1eWlKa1RLVFk4ZjI2Q0JZejZHZDduUTJQV3JDOAo=' | base64 -d)
-# echo $pat | docker login ghcr.io -u "TrustSECO-Machine-User" --password-stdin
-
 # pull and build docker image dependencies
 docker-compose -f docker-compose-deps.yml pull
 docker-compose -f docker-compose-deps.yml build
