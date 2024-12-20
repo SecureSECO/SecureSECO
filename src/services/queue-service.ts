7de22ac70b2be9ab3e500a98f0352144dc41efb9
Martijn Voordouw <martijnvoordouw@gmail.com>
2024-12-20 15:16:26 +0100

    made heap pop multiple items per block, not working currently
    sending multiple transactions per block seems to be difficult:
    - error with nonce number not being correct
    - fee being to low
    - other errors
    
    Fixing this is really difficult with the lisk documentattion being
    gone... might come back to it later

--- src/services/queue-service.ts
+++ src/services/queue-service.ts
@@ -1,15 +1,20 @@
 import Heap from 'heap-js';
 import Emitter from 'node:events';
+import fs from 'fs';
 import { QueueTransaction } from '../types';
 import {
     getAccount,
     getClient, getMinFee, getPassphrase, runTransaction, getMinimumBounty
 } from './dlt-service';
 import { encodeAndSign } from './add-job-service'
-import {CodaJob} from '../types'
+import { CodaJob } from '../types'
 
 const heap = new Heap<QueueTransaction>(comparator);
-var current_job: number | null = null;
+// Jobs that are waiting to be uploaded in the next block
+var current_jobs: number[] = [];
+// Keep track of payload size to avoid exceeding maximum size
+var current_payload_size: number = 0;
+var max_payload_size: number = 0;
 const emitter = new Emitter();
 
 export function addToHeap(transaction: QueueTransaction) {
@@ -31,28 +36,34 @@ export function clearQueue() {
 
 export async function startQueue() {
     const client = await getClient();
-
+    max_payload_size = (await client.node.getNodeInfo()).genesisConfig.maxPayloadLength;
     client.subscribe('app:block:new', async (event) => {
+        current_jobs = [];
+        current_payload_size = 0;
+        console.log("----------NEW BLOCK---------------------")
         await consumeFromHeap(client);
     });
 }
 
+/** Consumes transactions from the heap until either the heap is empty of if
+ * the payload limit has been reached for this block */
 async function consumeFromHeap(client) {
-    if (heap.isEmpty()) {
-        current_job = null;
-        return;
+    //while (!heap.isEmpty() && current_payload_size + transactionSize(heap.peek()) <= max_payload_size) {
+    for (let i = 0; i<2 && !heap.isEmpty(); i++) {
+        const queueTransaction = heap.pop();
+        current_payload_size += transactionSize(queueTransaction);
+        runQueuedTransactions(queueTransaction, client);
     }
+}
 
-    const queueTransaction = heap.pop();
-
+async function runQueuedTransactions(queueTransaction: QueueTransaction, client) {
     console.log(`Running transaction: ${queueTransaction.name}`);
 
     try {
         const data = queueTransaction.transaction.asset.data;
-        if (data)
-        {
+        if (data) {
             var bounty = (data as CodaJob).bounty;
-            current_job = (data as CodaJob).jobID;
+            current_jobs.push((data as CodaJob).jobID);
         }
         const { slingers } = await getAccount();
         const minimumBounty = BigInt(await getMinimumBounty());
@@ -63,7 +74,7 @@ async function consumeFromHeap(client) {
             return;
         }
 
-        if (bounty < minimumBounty){
+        if (bounty < minimumBounty) {
             console.log('Bounty to low, setting value to minimumBounty');
             let data = queueTransaction.transaction.asset.data;
             (data as CodaJob).bounty = BigInt(minimumBounty);
@@ -76,7 +87,9 @@ async function consumeFromHeap(client) {
         await runTransaction(transaction);
     } catch (e) {
         console.log('Encountered error, if you believe this was a mistake, please run task again.');
-        console.error(e, e.stack);
+        //console.error(e);
+        addToHeap(queueTransaction);
+        //console.error(e, e.stack);
     }
 }
 
@@ -89,13 +102,19 @@ function comparator(a: QueueTransaction, b: QueueTransaction) {
 }
 
 export function isJobInHeap(jobID: number): boolean {
-    return jobID === current_job || heap.toArray().some((queueTransaction) => {
+    return current_jobs.includes(jobID) || heap.toArray().some((queueTransaction) => {
         const data = queueTransaction.transaction.asset.data;
-        if (data){
+        if (data) {
             return (data as CodaJob).jobID === jobID;
         }
         return false;
     })
+}
+
+/** Calculates the size of a transaction, this can be used to avoid sending
+ * jobs that are to large for the chain. */
+function transactionSize(transaction: QueueTransaction): number {
+    return JSON.stringify(transaction.transaction).length;
 }
 
 /* This program has been developed by students from the bachelor Computer Science at Utrecht University within the Software Project course.
