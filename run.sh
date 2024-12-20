091a52aa33782d17c35bd3701a420d66b213687b
Martijn Voordouw <martijnvoordouw@gmail.com>
2024-12-20 15:33:27 +0100

    small fixes to run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
 if [ ! -x "$(command -v docker)" ]; then
     echo "Please make sure docker is installed."
     exit 0
 fi
 
-if [ ! -x "$(command -v docker-compose)" ]; then
+if [ "$(command -v docker-compose)" ]; then
+    COMPOSE_CMD="docker-compose"
+elif [ "$(command -v docker compose)" ]; then
+    COMPOSE_CMD="docker compose"
+else
     echo "Please make sure docker-compose is installed."
     exit 0
 fi
@@ -12,11 +16,11 @@ fi
 echo 'Starting up SecureSECO'
 
 # pull and build docker image dependencies
-docker-compose -f docker-compose-deps.yml pull
-docker-compose -f docker-compose-deps.yml build
+$COMPOSE_CMD -f docker-compose-deps.yml pull
+$COMPOSE_CMD -f docker-compose-deps.yml build
 
 # run docker containers
-docker-compose up --build
+$COMPOSE_CMD up --build
 
 # This program has been developed by students from the bachelor Computer Science at Utrecht University within the Software Project course.
 # © Copyright Utrecht University (Department of Information and Computing Sciences)
