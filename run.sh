ea0664b1a349fb358f0228ffd86fd862021b7ecb
Martijn Voordouw <martijnvoordouw@gmail.com>
2024-12-21 15:30:34 +0100

    removed docker compose v1 from run.sh file

--- run.sh
+++ run.sh
@@ -4,23 +4,19 @@ if [ ! -x "$(command -v docker)" ]; then
     exit 0
 fi
 
-if [ "$(command -v docker-compose)" ]; then
-    COMPOSE_CMD="docker-compose"
-elif [ "$(command -v docker compose)" ]; then
-    COMPOSE_CMD="docker compose"
-else
-    echo "Please make sure docker-compose is installed."
+if ! docker compose version &> /dev/null; then
+    echo "Please make sure docker compose is installed."
     exit 0
 fi
 
 echo 'Starting up SecureSECO'
 
 # pull and build docker image dependencies
-$COMPOSE_CMD -f docker-compose-deps.yml pull
-$COMPOSE_CMD -f docker-compose-deps.yml build
+docker compose -f docker-compose-deps.yml pull
+docker compose -f docker-compose-deps.yml build
 
 # run docker containers
-$COMPOSE_CMD up --build
+docker compose up --build
 
 # This program has been developed by students from the bachelor Computer Science at Utrecht University within the Software Project course.
 # © Copyright Utrecht University (Department of Information and Computing Sciences)
