9e9ac12baf5daf648fd4616491a55e6caa4fda3a
Martijn Voordouw <martijnvoordouw@gmail.com>
2025-08-27 19:22:36 +0200

    Moved top packages to dlt side

--- src/services/dlt-service.ts
+++ src/services/dlt-service.ts
@@ -162,79 +162,16 @@ export interface topPackageResult {
     packageName: string,
     packagePlatform: string,
     packageOwner: string,
-    packageRelease: string,
+    packageReleases: string[],
     score: number,
 }
 
 /*** Get the packages with either highest (descending = true) or lowest trust
- * scores. For every package only the version with the highest trust score is considered. */
+ * scores. */
 export async function getTopPackages(descending: boolean, count: number): Promise<topPackageResult[]> {
     const client = await getClient();
-    const packages: { packages: PackageData[] } = await client.invoke('packageData_getAllPackages');
-
-    // For each package get trust score
-    let packagesWithScore = (await Promise.all(
-        (packages as { packages: PackageData[] }).packages.map(async (pack) => {
-            let score = await getTrustScore(pack.packageName);
-            if (typeof score !== 'number') return null;
-            return {
-                packageName: pack.packageName,
-                packagePlatform: pack.packagePlatform,
-                packageOwner: pack.packageOwner,
-                packageRelease: pack.packageReleases[0],
-                score: score,
-            }
-        }))).filter((pack) => pack !== null);
-
-    let compareFn = (a, b) => descending ? b.score - a.score : a.score - b.score;
-    return get_top_elemenents(packagesWithScore, count, compareFn);
-}
-
-/** insert elem into a sorted list */
-function insert_into_sorted_list<Elem>(list: Elem[], elem: Elem, compareFn: (a: Elem, b: Elem) => number) {
-    list.push(elem);
-    let index = list.length - 1;
-    while (index > 0 && compareFn(elem, list[index - 1]) < 0) {
-        [list[index], list[index-1]] = [list[index - 1], list[index]]; // switch elements
-        index--;
-    }
-}
-
-/** Get the count top elements from the list, based on the comparison function */
-function get_top_elemenents<Elem>(list: Elem[], count: number, compareFn: (a: Elem, b: Elem) => number): Elem[] {
-    let result = [];
-    let i = 0;
-    while (i < list.length && result.length < count)
-    {
-        insert_into_sorted_list(result, list[i], compareFn);
-        i++;
-    }
-
-    if (result.length < count)
-        return result;
-
-    for (;i < list.length; i++)
-    {
-        let elem = list[i];
-        if (compareFn(elem, list[count - 1]) < 0) {
-            result.pop()
-            insert_into_sorted_list(result, elem, compareFn);
-        }
-    }
-    return result;
-}
-
-/** Return the version with the highest trust score of a specific package */
-export async function mostTrustedVersion(packageName: string, versions: string[]): Promise<{ version: string, score: number } | null> {
-    let best_version: null | { version: string, score: number } = null;
-    for (let version of versions) {
-        let score = await getTrustScore(packageName, version);
-        if (typeof score !== 'number') continue;
-        if (best_version === null || score <= best_version.score) {
-            best_version = { version, score };
-        }
-    }
-    return best_version
+    const { packages } = await client.invoke('trustfacts_getTopPackages', { descending, count }) as any;
+    return packages;
 }
 
 export async function getPackagesData(from?: number, count?: number, query?: string): Promise<{ packages: PackageData[], total: number }> {
